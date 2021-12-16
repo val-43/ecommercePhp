e3331191a11292b1ca843b35591e49ce08fe51dc
Valery <valery.debris@gmai.com>
2021-12-16 17:59:44 +0100

    first commit

---
+++ resources/templates/front/side_nav.php
@@ -0,0 +1,15 @@
+<div class="col-md-3">
+    <p class="lead">Shop Name</p>
+    <div class="list-group">
+        <?php
+            $query = "SELECT * FROM categories";
+            $send_query = mysqli_query($connection, $query);
+            if(!$send_query){
+                die("QUERY FAILED". mysqli_error($connection));
+            }
+            while($row = mysqli_fetch_array($send_query)){
+                echo "<a href='#' class='list-group-item'>{$row['cat_title']}</a>";
+            }
+        ?>
+    </div>
+</div>
