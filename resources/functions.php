77877f73e6db1a3426659b6e42cd202660ad1803
Valery <valery.debris@gmai.com>
2022-01-08 14:13:17 +0100

    avancement fonctions/affichage dynamique

--- resources/functions.php
+++ resources/functions.php
@@ -1,5 +1,7 @@
 <?php
 
+// fonctions utiles
+
 function redirect($location){
     header("Location: $location");
 }
@@ -11,17 +13,100 @@ function query($sql){
 
 function confirm($result){
     global $connection;
-    if($result){
+    if(!$result){
         die("QUERY FAILED" . mysqli_error($connection));
     }
 }
 
-function escape_string($string){
+function escape_string($string): string
+{
     global $connection;
     return mysqli_real_escape_string($connection, $string);
 }
 
 function fetch_array($result){
+
     return mysqli_fetch_array($result);
 }
+
+/***************************** FRONT END FUNCTIONS********************/
+
+// get products
+
+function get_products(){
+
+    $query=query(" SELECT * FROM products ");
+    confirm($query);
+
+    while ($row = fetch_array($query)){
+    $product = <<<DELIMETER
+
+         <div class="col-sm-4 col-lg-4 col-md-4">
+                                <div class="thumbnail">
+                                    <a href="item.php?id={$row['product_id']}"><img src="{$row['product_image']}" alt=""></a>
+                                    <div class="caption">
+                                        <h4 class="pull-right">{$row['product_price']} &#8364</h4>
+                                        <h4><a href="item.php?id={$row['product_id']}">{$row['product_title']}</a>
+                                        </h4>
+                                        <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                                        <a class="btn btn-primary" target="_blank" href="item.php?id={$row['product_id']}">Add to cart</a>
+                                    </div>
+                                </div>
+                            </div>
+
+DELIMETER;
+
+echo $product;
+    }
+
+}
+
+function get_categories(){
+
+    $query = query("SELECT * FROM categories");
+    confirm($query);
+
+    while($row = fetch_array($query)){
+
+        $category_links = <<<DELIMETER
+
+         <a href='category.php?id={$row['cat_id']}' class='list-group-item'>{$row['cat_title']}</a>
+
+DELIMETER;
+
+echo $category_links;
+    }
+}
+
+function get_products_in_cat_page()
+{
+
+    //error running this line of code
+    $id = escape_string($_GET['id']);
+    $query = query("SELECT * FROM products WHERE product_category_id = " . $id);
+
+    confirm($query);
+    while ($row = fetch_array($query)) {
+        $product = <<<DELIMETER
+            <div class="col-md-3 col-sm-6 hero-feature">
+                <div class="thumbnail">
+                    <img src="{$row['product_image']}" alt="">
+                    <div class="caption">
+                        <h3>{$row['product_title']}</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                        <p>
+                            <a href="#" class="btn btn-primary">Buy Now!</a> <a href="item.php?id={$row['product_id']}" class="btn btn-default">More Info</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+DELIMETER;
+        echo $product;
+    }
+}
+
+/***************************** BACK END FUNCTIONS********************/
+
+
+
 
--- resources/templates/front/side_nav.php
+++ resources/templates/front/side_nav.php
@@ -2,14 +2,9 @@
     <p class="lead">Shop Name</p>
     <div class="list-group">
         <?php
-            $query = "SELECT * FROM categories";
-            $send_query = mysqli_query($connection, $query);
-            if(!$send_query){
-                die("QUERY FAILED". mysqli_error($connection));
-            }
-            while($row = mysqli_fetch_array($send_query)){
-                echo "<a href='#' class='list-group-item'>{$row['cat_title']}</a>";
-            }
+
+        $category_links =true;
+        get_categories($category_links);
         ?>
     </div>
 </div>
